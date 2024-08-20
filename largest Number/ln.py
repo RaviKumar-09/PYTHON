8caecc442794a10fd03d103923c464abf3bb5ff9
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-20 21:55:45 +0530

    using Loops

--- largest Number/ln.py
+++ largest Number/ln.py
@@ -6,3 +6,14 @@ arr = [1,2,4,6,89]
 largest = max(arr)
 
 print(largest)
+
+
+# using loops
+
+arr = [1,34,55,668,2,1]
+largest = arr[0]
+
+for i in arr:
+    if i > largest:
+        largest = i
+print(largest)
