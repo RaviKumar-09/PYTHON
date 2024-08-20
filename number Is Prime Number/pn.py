f99ee0ca34fd9e4595d57ff8f168e37e1c189fe8
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-20 22:12:59 +0530

    True or False

--- number Is Prime Number/pn.py
+++ number Is Prime Number/pn.py
@@ -1,2 +1,12 @@
 # How to check if a number is prime number in Python
 
+def is_prime (n):
+    if n <= 1:
+        return False
+    
+    for i in range(2, int(n**0.5)+ 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(11))
