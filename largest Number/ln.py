512cea35be98c341546f17498a965094eccb335d
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-20 21:50:07 +0530

    using Max function

---
+++ largest Number/ln.py
@@ -0,0 +1,8 @@
+#how to print the largest number in array
+
+# using max () funtion
+
+arr = [1,2,4,6,89]
+largest = max(arr)
+
+print(largest)
--- largest Number/python.py
+++
@@ -1,1 +0,0 @@
-#how to print the largest number in array
