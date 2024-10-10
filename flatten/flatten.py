805ab855662de153512e07ca1ea2dca886b6d195
RaviKumar <ravikumarainapudi97@gmail.com>
2024-10-10 21:44:41 +0530

    You can flatten a list of lists using list comprehension.

--- flatten/flatten.py
+++ flatten/flatten.py
@@ -1,4 +1,4 @@
-list_of_lists = [[1, 2], [3, 4], [5, 6],[44.5,556]]
+list_of_lists = [[1, 2], [3, 4], [5, 6],[44.5,556],(667,900)]
 
 flat_list = [item for sublist in list_of_lists for item in sublist]
 print(flat_list) 
