1cdf571f456321f6e5dcdda26e52ff9c42e58ae8
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-06 19:45:16 +0530

    Python supports arithmetic, comparison, logical, and assignment operators.

---
+++ Basic Operators/bo.py
@@ -0,0 +1,13 @@
+a = 10
+b = 3
+
+# Arithmetic
+print(a + b)     # Addition: 13
+print(a ** b)    # Exponent: 1000
+
+# Comparison
+print(a > b)     # True
+print(a == b)    # False
+
+# Logical
+print(a > 5 and b < 5)  
