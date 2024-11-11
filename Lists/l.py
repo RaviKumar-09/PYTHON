2a5641ccd8695f3160dc6a178562967bf234a4f1
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-11 16:21:27 +0530

    Store multiple items.

---
+++ Lists/l.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  # Output: apple
+
+fruits[1] = "blueberry"
+fruits.append("date")
+print(fruits)  # Output: ['apple', 'blueberry', 'cherry', 'date']
