3834e9fbd01d1bc31e0ef53733c0f1b7f8f7dbc0
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-15 15:25:19 +0530

    Open and manipulate files in Python.

--- File Handling/fh.py
+++ File Handling/fh.py
@@ -3,6 +3,6 @@ with open("test.txt", "r") as file:
     file.write("Hello, World!")
 
 # Reading from a file
-with open("test.txt", "o") as file:
+with open("test.txt", "w") as file:
     content = file.read()
     print(content) 
