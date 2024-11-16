a78b8f93f68c502f3b0525cf2f1c1b1f615b311e
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-16 20:02:51 +0530

    Encapsulate data and functions.

--- Classes and Objects/co.py
+++ Classes and Objects/co.py
@@ -6,4 +6,4 @@ class Person:
     def greet(self):
         print(f"Hello, my name is {self.name}.")
 person1 = Person("Ravikumar", 25)
-person1.greet()  # Output: Hello, my name is Ravikumar.
+person1.greet() 
