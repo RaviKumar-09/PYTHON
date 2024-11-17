90be25218087fe39d3423757bd8f2788217e402f
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-17 20:24:03 +0530

    Loop through keys, values, or both.

---
+++ Dictionaries/dt.py
@@ -0,0 +1,13 @@
+person = {"name": "Ravikumar", "age": 25, "city": "Palacole"}
+
+# Loop through keys
+for key in person:
+    print(key)
+
+# Loop through values
+for value in person.values():
+    print(value)
+
+# Loop through key-value pairs
+for key, value in person.items():
+    print(f"{key}: {value}")
